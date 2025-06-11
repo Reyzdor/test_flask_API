3ff76ddfe4ac43163e6b55a073040040558c376e
Roman <m3f3dr0nbristleback@gmail.com>
2025-06-11 07:37:32 +0300

    UPDATE_SEARCH

--- app.py
+++ app.py
@@ -12,12 +12,25 @@ def get_all_data():
     return jsonify(data)
 
 
-@app.route("/api/data/<int:id>", methods=["GET"])
-def get_item(id):
-    item = next((item for item in data if item["id"] == id), None)
+@app.route("/api/data/<string:brand>", methods=["GET"])
+def get_item(brand):
+    item = next((item for item in data if item["Brand"].lower() == brand.lower()), None)
     if item:
         return jsonify(item)
     return jsonify({"error": "Not found"}), 404
+
+
+@app.route('/api/data/<string:brand>', methods=['DELETE'])
+def delete_item(brand):
+    global data
+    initial_length = len(data)
+    data = [item for item in data if item["Brand"].lower() != brand.lower()]
+
+    if len(data) < initial_length:
+        with open("data.json", "w") as f:
+            json.dump(data, f, indent=4)
+        return jsonify({f"message: {brand} deleted"}), 200
+    return jsonify({"error": "Brand not found"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
